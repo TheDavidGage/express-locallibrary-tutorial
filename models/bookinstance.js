265b23b5a2eea57ff8ee6bc31bf0d14cee48618c
David Gage Hall <FearlessDavidGage@TheDavidGage.local>
2017-11-15 20:19:13 -0500

    lots of stuff

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -3,32 +3,33 @@ var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
-var BookInstanceSchema = new Schema({
-    book: { type: Schema.ObjectId, ref: 'Book', required: true }, // reference the associated book
-    imprint: { type: String, required: true },
-    status: { type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance' },
+var BookInstanceSchema = Schema({
+    book: { type: Schema.ObjectId, ref: 'Book', required: true }, //reference to the associated book
+    imprint: {type: String, required: true},
+    status: {type: String, required: true, enum:['Available', 'Maintenance', 'Loaned', 'Reserved'], default:'Maintenance'},
     due_back: { type: Date, default: Date.now },
 });
 
-// Virtual for bookinstance' URL
+// Virtual for this bookinstance object's URL
 BookInstanceSchema
-    .virtual('url')
-    .get(function() {
-        return '/catalog/bookinstance/' + this._id;
-    });
+.virtual('url')
+.get(function () {
+  return '/catalog/bookinstance/'+this._id;
+});
 
-    BookInstanceSchema
-    .virtual('due_back_formatted')
-    .get(function () {
-      return moment(this.due_back).format('MMMM Do, YYYY');
-    });
 
-// Virtual for bookinstance due date
 BookInstanceSchema
-    .virtual('due_back_formatted')
-    .get(function() {
-        return moment(this.due_back).format('MMMM Do, YYYY');
-    });
+.virtual('due_back_formatted')
+.get(function () {
+  return moment(this.due_back).format('MMMM Do, YYYY');
+});
 
-// Export model
+BookInstanceSchema
+.virtual('due_back_yyyy_mm_dd')
+.get(function () {
+  return moment(this.due_back).format('YYYY-MM-DD');
+});
+
+
+//Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
--- routes/catalog.js
+++ routes/catalog.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-// Require controller modules
-var book_controller = require('../controllers/bookController');
+
+// Require our controllers
+var book_controller = require('../controllers/bookController'); 
 var author_controller = require('../controllers/authorController');
 var genre_controller = require('../controllers/genreController');
 var book_instance_controller = require('../controllers/bookinstanceController');
 
+
 /// BOOK ROUTES ///
 
 /* GET catalog home page. */
-router.get('/', book_controller.index);
+router.get('/', book_controller.index);  
 
 /* GET request for creating a Book. NOTE This must come before routes that display Book (uses id) */
 router.get('/book/create', book_controller.book_create_get);
@@ -33,12 +35,12 @@ router.post('/book/:id/update', book_controller.book_update_post);
 /* GET request for one Book. */
 router.get('/book/:id', book_controller.book_detail);
 
-/* GET request for list of all Book items. */
+/* GET request for list of all Book. */
 router.get('/books', book_controller.book_list);
 
 /// AUTHOR ROUTES ///
 
-/* GET request for creating Author. NOTE This must come before route for id (i.e. display author) */
+/* GET request for creating Author. NOTE This must come before route for id (ie display author)*/
 router.get('/author/create', author_controller.author_create_get);
 
 /* POST request for creating Author. */
@@ -61,6 +63,7 @@ router.get('/author/:id', author_controller.author_detail);
 
 /* GET request for list of all Authors. */
 router.get('/authors', author_controller.author_list);
+
 
 /// GENRE ROUTES ///
 
@@ -88,6 +91,7 @@ router.get('/genre/:id', genre_controller.genre_detail);
 /* GET request for list of all Genre. */
 router.get('/genres', genre_controller.genre_list);
 
+
 /// BOOKINSTANCE ROUTES ///
 
 /* GET request for creating a BookInstance. NOTE This must come before route that displays BookInstance (uses id) */
@@ -113,5 +117,6 @@ router.get('/bookinstance/:id', book_instance_controller.bookinstance_detail);
 
 /* GET request for list of all BookInstance. */
 router.get('/bookinstances', book_instance_controller.bookinstance_list);
+
 
 module.exports = router;
